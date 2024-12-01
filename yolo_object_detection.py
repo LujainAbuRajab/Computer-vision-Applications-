5ad22c7b01030b849ae65f9175123030ec1505d6
Lujain AbuRajab <102343947+LujainAbuRajab@users.noreply.github.com>
2024-12-02 00:49:26 +0300

    Update yolo_object_detection.py

--- yolo_object_detection.py
+++ yolo_object_detection.py
@@ -1,9 +1,8 @@
 from ultralytics import YOLO
 
-# Load YOLOv8 segmentation model
+# Load YOLOv8 object detection model
 model = YOLO('yolov8n.pt')
 
-# Run predictions on webcam (or replace '0' with a video file path)
-#model.predict(source=0, show=True, conf=0.4, stream=True)  # Corrected 'source' to integer 0 for webcam
+# Run predictions on webcam 
 model.predict(source=0, show=True, conf=0.4, device='cpu')
 
